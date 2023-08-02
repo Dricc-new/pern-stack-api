e562d0e1a13f09964e99a20d00de298bceacfe54
Dricc-new <diegoramoncc17@gmail.com>
2023-08-02 00:43:57 -0400

    using taskRouter on server

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import express from "express";
 import { PORT } from "./config";
+import morgan from "morgan";
+import taskRouter from "./routes/tacks.routes";
 
 const app = express()
+
+app.use(morgan('dev'))
+
+// Routers
+app.use(taskRouter)
 
 app.listen(PORT)
 console.log('Server on port', PORT)
